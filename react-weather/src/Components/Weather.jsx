19f9c51389e1fbcd335608d52fdbc0193238a296
Greg Munro <gregmunro90@gmail.com>
2023-10-20 17:35:07 +0200

    city name hard coded

--- react-weather/src/Components/Weather.jsx
+++ react-weather/src/Components/Weather.jsx
@@ -3,13 +3,12 @@ import axios from 'axios';
 
 const Weather = () => {
   const API_key = 'dba0838d8e60d5b708f95b93cf29fbd5'; // Replace with your actual API key
-  const lat = 39.099724; // Replace with the desired latitude
-  const lon = -94.578331; // Replace with the desired longitude
-  // const date = '16'; // Replace with the desired date
+  const city_name = 'Barcelona'
+
   const [weatherData, setWeatherData] = useState(null)
 
   useEffect(() => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`)
       .then((response) => {
         console.log('response', response.data);
         setWeatherData(response.data)
@@ -17,7 +16,7 @@ const Weather = () => {
       .catch((error) => {
         console.error('ERROR 🤦🏽‍♂️', error.message);
       });
-  }, [API_key, lat, lon]);
+  }, [API_key, city_name]);
 
   return (
     <div>
