854063373f88df1481f9258830ce158088b758c4
Greg Munro <gregmunro90@gmail.com>
2023-10-20 17:01:02 +0200

    init

---
+++ react-weather/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const openWeatherAPIKey = 'dba0838d8e60d5b708f95b93cf29fbd5'
+
+function App() {
+
+  return (
+    <>
+  <h1>{openWeatherAPIKey}</h1>
+    </>
+  )
+}
+
+export default App
