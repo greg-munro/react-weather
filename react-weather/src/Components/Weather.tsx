ef7a05a4c5b60986107e37f8283fc3e2dd111262
Greg Munro <gregmunro90@gmail.com>
2023-11-14 19:01:45 +0100

    v1

--- react-weather/src/Components/Weather.tsx
+++ react-weather/src/Components/Weather.tsx
@@ -1,27 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+
 
 const Weather = () => {
   const [location, setLocation] = useState('');
-  const API_KEY = '237c55e13ccd467290e170801231411'
+  const [weatherData, setWeatherData] = useState(null);
+  const API_KEY = '237c55e13ccd467290e170801231411';
 
   const handleSubmit = () => {
-    const userSearch = (document.querySelector('input') as HTMLInputElement).value
-    setLocation(userSearch || '')
+    const userSearch = (document.querySelector('input') as HTMLInputElement).value;
+    setLocation(userSearch || '');
   };
 
-  axios.get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no`)
-  .then(weatherData => console.log('data:', weatherData))
+  useEffect(() => {
+    if (location !== '') {
+      axios
+        .get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no`)
+        .then((response) => {
+          console.log('data:', response.data);
+          setWeatherData(response.data);
+        })
+        .catch((error) => console.error('Error fetching weather data:', error));
+    }
+  }, [location]);
 
+  let weatherIcon
+  if (weatherData?.current.condition.text === 'Partly cloudy' || 'Overcast') {
+    weatherIcon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Weather-overcast.svg/800px-Weather-overcast.svg.png'
+  }
+  else if (weatherData?.current.condition.text === 'Clear' ||  'Sunny') {
+    weatherIcon = 'https://static-00.iconduck.com/assets.00/weather-clear-symbolic-icon-2048x2048-v4afvu7m.png'
+  }
 
   return (
     <div className='main-search-box'>
-      <input
-        type='text'
-        placeholder='Enter location...'
-      />
+      <input type='text' placeholder='Enter location...' />
       <button onClick={handleSubmit}>Submit</button>
-      <h3>The weather in {location}</h3>
+      <p>The weather in {location.charAt(0).toUpperCase() + location.slice(1)}, {weatherData?.location.country}</p>
+      {weatherData && (
+        <div>
+          <p>{weatherData.current.temp_c}°C {weatherData.current.condition.text}</p>
+          <img src={weatherIcon} className="weather-icon" />
+        </div>
+      )}
     </div>
   );
 };
