927a10a7d62c9c315f8456e9cac71214438e810a
Greg Munro <gregmunro90@gmail.com>
2023-11-17 15:29:42 +0100

    header removed

--- react-weather/src/App.jsx
+++ react-weather/src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css'
 import Weather from './Components/Weather'
-import Header from './Components/Header'
 
 function App() {
   return (
     <>
-  
       <div className='main-container'>
-        <Header />
         <Weather />
       </div>
 
