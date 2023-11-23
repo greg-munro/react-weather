6e265ad4f816f05d4646a885bba64b984eed569a
Greg Munro <gregmunro90@gmail.com>
2023-11-23 13:51:06 +0100

    search icon active inprogress

--- react-weather/src/Components/Weather.jsx
+++ react-weather/src/Components/Weather.jsx
@@ -8,6 +8,8 @@ import WindChart from './WindChart';
 import { Switch, Stack } from '@chakra-ui/react';
 import Clouds from './Clouds';
 import DailyHourlySelector from './DailyHourlySelector';
+import { FaSearch } from 'react-icons/fa';
+
 
 const Weather = () => {
   const [location, setLocation] = useState('');
@@ -21,6 +23,7 @@ const Weather = () => {
   const [isCelsius, setIsCelsius] = useState(true);
   const [error, setError] = useState(null);
 
+
   const API_KEY = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
 
   const handleSubmit = () => {
@@ -28,11 +31,20 @@ const Weather = () => {
     const formGroup = document.querySelector('.form__group');
     if (userSearch.length > 0) {
       setLocation(userSearch);
-      formGroup.style.position = 'absolute';
-      formGroup.style.top = 0;
+      formGroup.classList.add('search-icon-active')
+      formGroup.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
+      <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+      </svg>`
+      formGroup.addEventListener('click', handleSearchIconClick)
     }
     setError(null);
   };
+
+  const handleSearchIconClick = () => {
+    console.log('search clicked')
+    const formGroup = document.querySelector('.form__group');
+    formGroup.classList.remove('search-icon-active')
+  }
 
   const handleReturn = (event) => {
     if (event.key === 'Enter') {
